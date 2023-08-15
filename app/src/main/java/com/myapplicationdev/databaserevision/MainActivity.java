7647427280452598e8cb94267b08f981c813a44f
elvinacheong <22016916@myrp.edu.sg>
2023-08-15 12:42:41 +0800

    elvina MainActivity and activity_main

--- app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
@@ -2,6 +2,7 @@ package com.myapplicationdev.databaserevision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnInsertRecord, btnRetrieveRecords;
+    Button btnInsertRecord, btnRetrieveRecords, btnRetrieveRecords2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +19,29 @@ public class MainActivity extends AppCompatActivity {
 
         btnInsertRecord = findViewById(R.id.btnInsertRecord);
         btnRetrieveRecords = findViewById(R.id.btnGetRecord);
+        btnRetrieveRecords2 = findViewById(R.id.btnGetRecord2);
 
         btnInsertRecord.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
-
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(intent);
             }
         });
 
         btnRetrieveRecords.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RetrieveActivityListView.class);
+                startActivity(intent);
+            }
+        });
 
+        btnRetrieveRecords2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RetrieveActivityTextView.class);
+                startActivity(intent);
             }
         });
 
